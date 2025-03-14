98bd1734b70be26d2e6320f60af3e407d7771a8d
marlenepaper <marlenepaper@hotmail.com>
2025-03-14 21:30:36 +0100

    falta instalar libreria

--- app/presentation/navigation/TabNavigator.tsx
+++ app/presentation/navigation/TabNavigator.tsx
@@ -1,53 +1,70 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Image } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // Importa las pantallas desde tu estructura
 import HomeScreen from '../views/mainNav/home/Home';
 import TheatreInfoScreen from '../views/mainNav/theatreInfo/TheatreInfo';
 import TicketsListScreen from '../views/mainNav/ticketsList/TicketList';
 import UserProfileScreen from '../views/mainNav/userProfile/UserProfile';
+import TicketListScreen from "../views/mainNav/ticketsList/TicketList";
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    position: 'absolute',
+                    backgroundColor: 'white',
+                    height: 90,
+                    borderTopLeftRadius: 25,
+                    borderTopRightRadius: 25,
+                    shadowColor: "#000",
+                    shadowOffset: { width: 0, height: -3 },
+                    shadowOpacity: 0.1,
+                    shadowRadius: 4,
+                    elevation: 5,
+                },
+                tabBarLabelStyle: {
+                    fontSize: 12,
+                    fontWeight: '600',
+                    marginBottom: 10,
+                },
+            }}><Tab.Screen
+            name="Cartelera"
+            component={HomeScreen}
+            options={{
+                tabBarLabel: "Cartelera",
+                tabBarIcon: ({ color }) => (
+                    <FontAwesome name="film" size={25} color={color} />
+                )
+            }}
+        />
             <Tab.Screen
-                name="Home"
-                component={HomeScreen}
+                name="Entradas"
+                component={TicketListScreen}
                 options={{
-                    title: "Lista de categorías",
-                    tabBarLabel: "Inicio",
+                    tabBarLabel: "Entradas",
                     tabBarIcon: ({ color }) => (
-                        <Image source={require("../../../assets/icons/eye.png")}
-                               style={{ width: 25, height: 25, tintColor: color }} />
+                        <Ionicons name="qr-code-outline" size={25} color={color} />
                     )
                 }}
             />
             <Tab.Screen
-                name="TheatreInfo"
-                component={TheatreInfoScreen}
-                options={{
-                    title: "Pedidos realizados",
-                    tabBarLabel: "Pedidos",
-                    tabBarIcon: ({ color }) => (
-                        <Image source={require("../../../assets/icons/eye.png")}
-                               style={{ width: 25, height: 25, tintColor: color }} />
-                    )
-                }}
-            />
-            <Tab.Screen
-                name="UserProfile"
+                name="Perfil"
                 component={UserProfileScreen}
                 options={{
-                    title: "Mi perfil",
-                    tabBarLabel: "Mi perfil",
+                    tabBarLabel: "Perfil",
                     tabBarIcon: ({ color }) => (
-                        <Image source={require("../../../assets/icons/eye.png")}
-                               style={{ width: 25, height: 25, tintColor: color }} />
+                        <Feather name="user" size={25} color={color} />
                     )
                 }}
             />
         </Tab.Navigator>
     );
-}
+};
