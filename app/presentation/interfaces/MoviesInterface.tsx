2e2fbf9a2aad0360d6c28ce8c4086090c061e429
marlenepaper <marlenepaper@hotmail.com>
2025-03-25 23:15:00 +0100

    ....

--- app/presentation/interfaces/MoviesInterface.tsx
+++ app/presentation/interfaces/MoviesInterface.tsx
@@ -1,20 +1,18 @@
-export interface FuncionDTO {
-    id: number;
-    fechaHora: string;
-    sala: string;
+export interface MovieBoxInterface{
+    age: number,
+    name: string,
+    duration: string,
+    version: string,
+    date: string,
+    time: string,
+    room: number
 }
 
-export interface PeliculaDTOInterface {
-    id: number;
-    nombre: string;
-    anio: number;
-    duracion: number;
-    sinopsis: string;
-    imagenPoster: string;
-    categoria: string;
-    clasificacion: string;
-    formato: string;
-    lenguaje: string;
-    color: string;
-    funciones: FuncionDTO[];
+export interface MovieDataInterface{
+    age: number,
+    name: string,
+    category: string,
+    year: string,
+    duration: string,
+    version: string,
 }
